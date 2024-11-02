ff637c17bb8b24c467336d768775a2e5b3f1d64c
Aral Roca <aral-rg@hotmail.com>
2024-11-02 15:06:23 +0100

    test: add e2e to pipeline

---
+++ e2e/examples.test.ts
@@ -0,0 +1,71 @@
+import { describe, it , expect } from 'bun:test'
+import fs from 'node:fs'
+import path from 'node:path'
+
+describe('e2e examples', () => {
+  describe('with-api-routes', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-elysia', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-external-web-component', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-i18n', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-middleware', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-pandacss', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-sqlite-with-server-action', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-streaming-list', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-suspense', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-tailwindcss', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+  
+  describe('with-view-transitions', () => {
+    it('todo', () => {
+      expect(true).toBeTrue()
+    })
+  })
+})
