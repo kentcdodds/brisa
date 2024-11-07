5a1b0bb519b2213138fd5a5c5697d40a336a70de
Aral Roca <aral-rg@hotmail.com>
2024-11-07 16:44:18 +0100

    fix: fix e2e

--- e2e/examples.test.ts
+++ e2e/examples.test.ts
@@ -1,10 +1,22 @@
-import { describe, it , expect, beforeAll, afterAll } from 'bun:test'
-import { $, spawn } from 'bun'
-import path from 'node:path'
-import { chromium, firefox, webkit } from "playwright";
+import {
+  describe,
+  beforeAll,
+  afterAll,
+  afterEach,
+  beforeEach,
+  expect,
+  it,
+} from 'bun:test';
+import { $, spawn } from 'bun';
+import path from 'node:path';
+import { Browser, chromium, firefox, Page, webkit } from 'playwright';
 
-const browsers = [chromium, firefox, webkit]
-const timeout = 30000
+const engine: Record<string, any> = {
+  chrome: chromium,
+  firefox: firefox,
+  safari: webkit,
+};
+const timeout = 30000;
 const examples = [
   { name: 'with-api-routes' },
   { name: 'with-elysia' },
@@ -16,58 +28,88 @@ const examples = [
   { name: 'with-streaming-list' },
   { name: 'with-suspense' },
   { name: 'with-tailwindcss' },
-  { name: 'with-view-transitions' }
-]
+  { name: 'with-view-transitions' },
+];
 
-describe('e2e examples', () => {
-  for (const example of examples) {
-    describe(example.name, () => {
-      const { setup, teardown, origin } = prepareProject(example.name);
+describe.each(examples)('e2e example', (example) => {
+  describe(example.name, () => {
+    const { setup, teardown, origin } = prepareProject(example.name);
 
-      beforeAll(setup);
-      afterAll(teardown);
+    beforeAll(setup);
+    afterAll(teardown);
 
-      for (const browserType of browsers) {
-        it(`should load home page with status 200 and HTML content on ${browserType.name()}`, async () => {
-          const browser = await browserType.launch();
-          try {
-            const page = await browser.newPage();
-            const response = await page.goto(origin, { timeout });
-            expect(response).not.toBeNull();
-            expect(response!.status()).toBe(200);
-            expect(await response!.headerValue("content-type")).toContain("text/html");
-         } finally {
-            await browser.close();
-         }
+    describe.each(['chrome', 'firefox', 'safari'])('%s', (browserName) => {
+      let browser: Browser;
+      let page: Page;
+
+      beforeAll(async () => {
+        browser = await engine[browserName].launch();
+      });
+
+      beforeEach(async () => {
+        page = await browser.newPage();
+      });
+
+      afterAll(async () => {
+        await browser?.close?.();
+      });
+
+      afterEach(async () => {
+        await page?.close?.();
+      });
+
+      it(`should load home page with status 200 and HTML content`, async () => {
+        const response = await page.goto(origin, {
+          waitUntil: 'load',
+          timeout,
         });
-      }
+        expect(response).not.toBeNull();
+        expect(response!.status()).toBe(200);
+        expect(await response!.headerValue('content-type')).toContain(
+          'text/html',
+        );
+        expect(await (await page.$('body'))?.innerHTML()).toMatchSnapshot();
+      });
     });
-  }
-})
+  });
+});
 
 function prepareProject(exampleName: string) {
-  let serverProcess
+  let serverProcess;
 
-  globalThis.currentPort = globalThis.currentPort ? globalThis.currentPort + 1 : 3000
+  globalThis.currentPort = globalThis.currentPort
+    ? globalThis.currentPort + 1
+    : 3000;
 
   async function setup() {
-    const exampleDir = path.join(import.meta.dir, '..', 'examples', exampleName);
+    const exampleDir = path.join(
+      import.meta.dir,
+      '..',
+      'examples',
+      exampleName,
+    );
 
-    console.log(`✅ Setting up ${exampleName} example...`);
+    console.log(`\t→ Setting up ${exampleName} example...`);
     await $`cd ${exampleDir} && bun i && bun run build`;
 
     serverProcess = spawn({
-      cmd: ["bun", "start", "-p", globalThis.currentPort.toString()],
+      cmd: ['bun', 'start', '-p', globalThis.currentPort.toString()],
       cwd: exampleDir,
-      stdout: "inherit",
-      stderr: "inherit",
+      stdout: 'inherit',
+      stderr: 'inherit',
     });
+
+    await Bun.sleep(2000);
   }
 
   async function teardown() {
-    console.log(`✅ Tearing down ${exampleName} example...`);
-    await serverProcess.kill('SIGINT')
+    console.log(`\t→ Tearing down ${exampleName} example...`);
+    await serverProcess.kill('SIGINT');
   }
 
-  return { setup, teardown, origin: `http://localhost:${globalThis.currentPort}` }
+  return {
+    setup,
+    teardown,
+    origin: `http://localhost:${globalThis.currentPort}`,
+  };
 }
