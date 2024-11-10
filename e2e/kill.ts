5d21aa7e79230ab78e626084f44a831c7a1a540a
Aral Roca <aral-rg@hotmail.com>
2024-11-10 15:29:59 +0100

    test: add process.exit

--- e2e/kill.ts
+++ e2e/kill.ts
@@ -2,8 +2,12 @@ import { $ } from 'bun';
 
 console.log('🎉 Killing all bun processes...');
 
-if (process.platform === 'win32') {
-  await $`taskkill /IM bun.exe /F`.quiet();
-} else {
-  await $`pkill -f bun`.quiet();
+try {
+  if (process.platform === 'win32') {
+    await $`taskkill /IM bun.exe /F`.quiet();
+  } else {
+    await $`pkill -f bun`.quiet();
+  }
+} finally {
+  process.exit(0);
 }
